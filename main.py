c5e86bf006b775acd7c05c6ea8cdde0c33e48335
Anshkumar Patel <anshpatel274006@gmail.com>
2025-12-17 19:02:47 +0000

    refactor user registration and authentication logic; migrate user data to database

--- main.py
+++ main.py
@@ -1,56 +1,67 @@
+import bcrypt
 
-import sqlite3
-import pandas as pd
+# hashed using bcrypt
+def generate_hash(psw):
+    byte_psw = psw.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(byte_psw, salt)
+    return hash.decode('utf-8')
 
-from app_model.db import conn
-from app_model.users import add_user, get_user
-from hashing import generate_hash, is_valid_hash
+# validating hash vs psw
+def is_valid_hash(psw, hash):
+    hash_ = hash.encode('utf-8')
+    byte_psw = psw.encode('utf-8')
+    is_valid = bcrypt.checkpw(byte_psw, hash_)
+    return is_valid
 
 
-# user registration
-def register_user(conn):
+
+def register_user():
+    name = input('Enter your name: > ')
+    password = input('enter your password: >')
+    hash_password = generate_hash(password)
+    with open('users.txt', 'a') as f:
+        f.write(f'{name},{hash_password}\n')
+    print('User successfully register !!')
+
+
+# user log in
+
+def log_in_user():
     name = input('Enter your name: > ')
     password = input('Enter your password: > ')
-    hash_password = generate_hash(password)
-    add_user(conn, name, hash_password)
-
-#user log-in 
-def log_in_user(conn):
-    name = input('Enter your name :')
-    password = input('Enter your password :')
-    id, user_name, user_hash = get_user(conn, name)
-    if name == user_name and is_valid_hash(password, user_hash):
-        return True
+    with open('users.txt', 'r') as f:
+        users = f.readlines()
+    for user in users:
+        user_name, user_hash = user.strip().split(',')
+        if name == user_name and is_valid_hash(password, user_hash):
+            return True
     return False
+
+
 
 def main():
     while True:
-        print('Welcome to the system !')
-        print('Choose from the following options :')
+        print('Welcome to the system!!')
+        print('Choose from the following options:')
         print('1. To Register')
         print('2. To Log in')
-        print('3. To Exit')
+        print('3. To Exit: ')
 
-        choice = input(':')
+        choice = input(': > ')
+
         if choice == '1':
             register_user()
-        elif choice =='2':
+        elif choice == '2':
             if log_in_user():
-                print('Login Successfull')
+                print('Log in successfully! ')
             else:
-                print('Incorrect log in. Please try again')
-        elif choice =='3':
-            print('Good Bye!!')
+                print('Incorrect log in. Try again !')
+        elif choice == '3':
+            print('Good bye!!')
             break
+
 
 if __name__ == '__main__':
     main()
 
-
-#create db
-conn = sqlite3.connect('DATA/project_data.db')
-cur = conn.cursor()
-sql = ''
-cur.execute(sql)
-conn.commit()
-conn.close()
