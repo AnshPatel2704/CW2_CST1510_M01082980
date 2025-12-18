327402a955aac1b2b693da3afe4f7f935f5b1126
Anshkumar Patel <anshpatel274006@gmail.com>
2025-12-18 11:27:31 +0000

    change

--- hashing.py
+++ hashing.py
@@ -1,13 +1,12 @@
 import bcrypt
 def generate_hash(psw):
-    byte_psw = psw.encode('utf-8')     # str → bytes ✅
+    byte_psw = psw.encode('utf-8')    
     salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(byte_psw, salt)  # returns bytes ✅
-    return hash.decode('utf-8')        # bytes → str ✅
-
+    hash = bcrypt.hashpw(byte_psw, salt)  
+    return hash.decode('utf-8')        
 
 def is_valid_hash(psw, hash):
-    hash_ = hash.encode('utf-8')       # str → bytes ✅
+    hash_ = hash.encode('utf-8')     
     byte_psw = psw.encode('utf-8')
     is_valid = bcrypt.checkpw(byte_psw, hash_)
     return is_valid
