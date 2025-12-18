cad6e3c6cd42bc6f1edeb2dedf8b4de58e651b09
Anshkumar Patel <anshpatel274006@gmail.com>
2025-12-18 11:16:37 +0000

    New changes

--- main.py
+++ main.py
@@ -1,41 +1,30 @@
 import bcrypt
 import sqlite3
+import pandas as pd
 
-# hashed using bcrypt
-def generate_hash(psw):
-    byte_psw = psw.encode('utf-8')
-    salt = bcrypt.gensalt()
-    hash = bcrypt.hashpw(byte_psw, salt)
-    return hash.decode('utf-8')
+from app_model.users import add_user, migrate_users, get_all_users, get_user, update_user, delete_user  
+from app_model.db import get_connection
+from hashing import generate_hash, is_valid_hash
 
-# validating hash vs psw
-def is_valid_hash(psw, hash):
-    hash_ = hash.encode('utf-8')
-    byte_psw = psw.encode('utf-8')
-    is_valid = bcrypt.checkpw(byte_psw, hash_)
-    return is_valid
+conn = get_connection()
 
 
-
-def register_user():
+def register_user(conn):
     name = input('Enter your name: > ')
     password = input('enter your password: >')
     hash_password = generate_hash(password)
-    with open('users.txt', 'a') as f:
-        f.write(f'{name},{hash_password}\n')
-    print('User successfully register !!')
+    add_user(conn, name, hash_password)
+
 
 
 # user log in
 
-def log_in_user():
+def log_in_user(conn):
     name = input('Enter your name: > ')
     password = input('Enter your password: > ')
-    with open('users.txt', 'r') as f:
-        users = f.readlines()
-    for user in users:
-        user_name, user_hash = user.strip().split(',')
-        if name == user_name and is_valid_hash(password, user_hash):
+    id, user_name, user_hash =get_user(conn, name)
+    print(f'Welcome {user_name}!')
+    if name == user_name and is_valid_hash(password, user_hash):
             return True
     return False
 
@@ -52,9 +41,9 @@ def main():
         choice = input(': > ')
 
         if choice == '1':
-            register_user()
+            register_user(conn)
         elif choice == '2':
-            if log_in_user():
+            if log_in_user(conn):
                 print('Log in successfully! ')
             else:
                 print('Incorrect log in. Try again !')
@@ -62,20 +51,5 @@ def main():
             print('Good bye!!')
             break
 
-
-# create db insert, delete, update
-conn = sqlite3.connect('DATA/project_data.db')
-cur = conn.cursor()
-
-sql = '''CREATE TABLE users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password_hash TEXT NOT NULL);
-'''
-
-cur.execute(sql)
-conn.commit()
-
-conn.close()
-
-
+if __name__ == '__main__':
+    main()
